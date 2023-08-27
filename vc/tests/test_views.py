9c4e5e8b3e83f5eb7baacd0478d721521071522b
DrCBeatz <brynecarruthers@gmail.com>
2023-08-27 16:45:24 -0400

    split tests into models & views

--- vc/tests/test_views.py
+++ vc/tests/test_views.py
@@ -22,6 +22,9 @@ def generate_pdf_content(content="this is a test pdf document"):
     return buffer
 
 
+# Fixtures
+
+
 @pytest.fixture
 def user(db):
     User = get_user_model()
@@ -83,118 +86,6 @@ def documents(db, user, experts):
             embeddings="embeddings/mingyur_rinpoche_embeddings.parquet",
         ),
     ]
-
-
-# Model tests
-
-
-@pytest.mark.django_db
-def test_create_model():
-    model = Model.objects.create(
-        name="TestModel",
-        input_token_cost=10.0,
-        output_token_cost=5.0,
-        context_length=20,
-    )
-    assert Model.objects.count() == 1
-    assert model.name == "TestModel"
-
-
-@pytest.mark.django_db
-def test_create_expert():
-    model = Model.objects.create(
-        name="TestModel",
-        input_token_cost=10.0,
-        output_token_cost=5.0,
-        context_length=20,
-    )
-    expert = Expert.objects.create(
-        name="TestExpert", prompt="This is a test prompt", model=model
-    )
-    assert Expert.objects.count() == 1
-    assert expert.name == "TestExpert"
-
-
-@pytest.mark.django_db
-def test_create_expert():
-    model = Model.objects.create(
-        name="TestModel",
-        input_token_cost=10.0,
-        output_token_cost=5.0,
-        context_length=20,
-    )
-    expert = Expert.objects.create(
-        name="TestExpert", prompt="This is a test prompt", model=model
-    )
-    assert Expert.objects.count() == 1
-    assert expert.name == "TestExpert"
-    assert expert.model == model
-
-
-@pytest.mark.django_db
-def test_create_conversation():
-    User = get_user_model()
-    user = User.objects.create_user(username="testuser", password="testpass123")
-    model = Model.objects.create(
-        name="TestModel",
-        input_token_cost=10.0,
-        output_token_cost=5.0,
-        context_length=20,
-    )
-    expert = Expert.objects.create(name="TestExpert", model=model)
-    conversation = Conversation.objects.create(
-        title="TestConversation", expert=expert, user=user
-    )
-    assert Conversation.objects.count() == 1
-    assert conversation.title == "TestConversation"
-
-
-@pytest.mark.django_db
-def test_create_message():
-    User = get_user_model()
-    user = User.objects.create_user(username="testuser", password="testpass123")
-    model = Model.objects.create(
-        name="TestModel",
-        input_token_cost=10.0,
-        output_token_cost=5.0,
-        context_length=20,
-    )
-    expert = Expert.objects.create(name="TestExpert", model=model)
-    conversation = Conversation.objects.create(
-        title="TestConversation", expert=expert, user=user
-    )
-    message = Message.objects.create(
-        conversation=conversation, question="TestQuestion", answer="TestAnswer"
-    )
-    assert Message.objects.count() == 1
-    assert message.question == "TestQuestion"
-
-
-def get_mock_response(content):
-    """Helper function to mock HTTP responses."""
-    response = mock.Mock()
-    response.content = content.encode()
-    return response
-
-
-@pytest.mark.django_db
-def test_document_save_with_html_content(experts):
-    # Mock the external call to get the HTML content
-    with mock.patch(
-        "requests.get",
-        return_value=get_mock_response("<html><body>Hello, World!</body></html>"),
-    ):
-        doc = Document(
-            title="Test Doc", expert=experts[0], html_url="http://example.com"
-        )
-        doc.save()
-
-        assert doc.content == "Hello, World!"
-
-
-def test_has_content_changed_new_instance():
-    doc = Document(title="Test Doc", content="Test content")
-    assert doc._has_content_changed() is True
 
 
 # views tests
